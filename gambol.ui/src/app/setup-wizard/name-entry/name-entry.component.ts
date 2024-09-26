599272cd987904235841b545702bbaaaa90171dd
Prabhjot Singh <97115364+PrabhjotxSingh@users.noreply.github.com>
2024-09-26 03:33:51 -0400

    Added some basic alerts

--- gambol.ui/src/app/setup-wizard/name-entry/name-entry.component.ts
+++ gambol.ui/src/app/setup-wizard/name-entry/name-entry.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
+import Swal from 'sweetalert2';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-name-entry',
   templateUrl: './name-entry.component.html',
-  styleUrls: ['./name-entry.component.css']
+  styleUrls: ['./name-entry.component.css'],
 })
 export class NameEntryComponent {
+  userName: string = '';
+  recalledName: string | null = '';
 
+  constructor(private router: Router) {}
+
+  submitName() {
+    if (this.userName.trim() == '') {
+      Swal.fire({
+        title: 'Error!',
+        text: 'Looks like your username was not set!',
+        icon: 'error',
+        confirmButtonText: 'Try Again',
+        confirmButtonColor: '#000',
+      });
+    } else {
+      console.log('Save logic');
+      this.router.navigate(['setup-wizard/color-entry/']);
+    }
+  }
 }
--- gambol.ui/src/app/setup-wizard/setup-wizard.module.ts
+++ gambol.ui/src/app/setup-wizard/setup-wizard.module.ts
@@ -6,9 +6,16 @@ import { StartSetupComponent } from './start-setup/start-setup.component';
 import { NameEntryComponent } from './name-entry/name-entry.component';
 import { ColorEntryComponent } from './color-entry/color-entry.component';
 import { EndSetupComponent } from './end-setup/end-setup.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [SetupWizardComponent, StartSetupComponent, NameEntryComponent, ColorEntryComponent, EndSetupComponent],
-  imports: [CommonModule, AppRoutingModule],
+  declarations: [
+    SetupWizardComponent,
+    StartSetupComponent,
+    NameEntryComponent,
+    ColorEntryComponent,
+    EndSetupComponent,
+  ],
+  imports: [CommonModule, AppRoutingModule, FormsModule],
 })
 export class SetupWizardModule {}
